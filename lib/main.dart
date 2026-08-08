995bc5a3397e0b79ec246c9a91b24e65534b3190
Bhaveshupadhyay <bhaveshupadhyay929@gmail.com>
2026-08-08 08:41:44 +0530

    Disable reCAPTCHA app verification in debug mode to bypass RecaptchaCallWrapper Android error

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'app/di/service_locator.dart';
 import 'app/theme/app_theme.dart';
@@ -14,6 +16,11 @@ void main() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+    if (kDebugMode) {
+      await FirebaseAuth.instance.setSettings(
+        appVerificationDisabledForTesting: true,
+      );
+    }
   } catch (e) {
     debugPrint('Firebase initialization warning: $e');
   }
